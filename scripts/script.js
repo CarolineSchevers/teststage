20ccdc6b516b36038fccd60821e00319fd2dcf26
CarolineSchevers <scheverscaroline@gmail.com>
2019-10-22 21:00:56 +0200

    mobile: add toggle to navbar

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function toggleNav() {
+    document.getElementById("nav-items").classList.toggle("show");
+}
+
+function showInfo() {
+    document.getElementById("data-info").classList.toggle("show");
+}
